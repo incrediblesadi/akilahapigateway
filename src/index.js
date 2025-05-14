5a2e50620020850be27c08b784c43c3d4cd2f413
incrediblesadi <info@incrediblesadi.com>
2025-05-14 19:44:31 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-`const PORT = process.env.PORT|| 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
   res.send('Server is running.');
 });
 
