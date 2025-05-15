9754cc3b826eaff6ef8d6f6de4e209f4b80e9869
incrediblesadi <info@incrediblesadi.com>
2025-05-14 20:16:51 -0400

    Update firebase.js to use correct Realtime Database URL (sadiworkspace)

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ const admin = require('f!");
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://akilahstack.firebaseio.com'
+  databaseURL: 'https://sadiworkspace.firebaseiod.com'
 });
 
 const db = admin.database();
