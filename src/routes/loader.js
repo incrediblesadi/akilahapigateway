671d5e65f9c01c30027a87d9a3c1b39b196fa70c
incrediblesadi <info@incrediblesadi.com>
2025-05-14 20:30:59 -0400

    Add central route loader to organize modular route imports

---
+++ src/routes/loader.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const firebaseLogger = require('./firebase/logger');
+
+const router = express.Router();
+
+// Mount Firebase-related routes
+router.use('/firebase', firebaseLogger);
+
+// Add other routes here later
+
+module.exports = router;
