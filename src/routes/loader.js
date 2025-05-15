6e23342d5e64d9ff295d4f556d1c6e4e56646572
incrediblesadi <info@incrediblesadi.com>
2025-05-14 23:57:16 -0400

    Update loader.js

--- src/routes/loader.js
+++ src/routes/loader.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const firebaseLogger = require('./logger');
+const notesRoute = require('./notes');
 
 const router = express.Router();
 
-// Mount logger route at /firebase/logger
+// Mount logger and notes routes at /firebase/*
 router.use('/firebase', firebaseLogger);
+router.use('/firebase', notesRoute);
 
 module.exports = router;
