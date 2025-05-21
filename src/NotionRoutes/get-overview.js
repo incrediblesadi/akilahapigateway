d23ed2b57a6c40e965e6a6638894136b3dd20033
incrediblesadi <info@incrediblesadi.com>
2025-05-20 20:51:37 -0400

    Fix syntax error in get-overview.js for dynamic property access on block text

--- src/NotionRoutes/get-overview.js
+++ src/NotionRoutes/get-overview.js
@@ -18,7 +18,7 @@ router.get('/get-overview', async (req, res) => {
         id: page.id,
         title: page.label,
         blockTypes: blocksRes.results.map(n => n.type),
-        blocks: blocksRes.results.map(b => ({ id: b.id, type: b.type, text: b.[b.type]?.rich_text[0]?.plain_text || '' })
+        blocks: blocksRes.results.map(b => ({ id: b.id, type: b.type, text: b[b.type]?.rich_text?.0[]?.plain_text || '''})
     });
   }
 
